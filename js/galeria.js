c1a6d7f698936e8090a2345872fce62f490dd15e
Cristian Rodriguez <chipicris8690@hotmail.com>
2021-07-18 23:46:55 -0300

    ..

--- js/galeria.js
+++ js/galeria.js
@@ -2,23 +2,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
     crearGaleria();
 })
 
+
 function crearGaleria(){
     let node=document.querySelector(".galeria__imagenes");
     for(let i=1;i<=12;i++){
         let img=document.createElement("img");
         img.src=`../src/build/img/thumb/${i}.webp`;
         img.className="imagen__galeria";
+        img.addEventListener('click',mostrarImagen);
         node.appendChild(document.createElement('li')).appendChild(img);
     }
-    
 }
 
-let img=document.querySelectorAll('.imagen__galeria');
 function mostrarImagen(){
     alert('Diste click');
 }
-img.addEventListener('click',mostrarImagen)
-
 
 
 
