2f5549bb35429faa50c2c64f29e8f9f146d257a0
Cristian Rodriguez <chipicris8690@hotmail.com>
2021-07-18 23:44:10 -0300

    Se agregaron eventos a las imagenes

--- js/galeria.js
+++ js/galeria.js
@@ -1,11 +1,24 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    crearGaleria();
+})
+
 function crearGaleria(){
     let node=document.querySelector(".galeria__imagenes");
     for(let i=1;i<=12;i++){
         let img=document.createElement("img");
         img.src=`../src/build/img/thumb/${i}.webp`;
+        img.className="imagen__galeria";
         node.appendChild(document.createElement('li')).appendChild(img);
     }
     
 }
 
-crearGaleria();
+let img=document.querySelectorAll('.imagen__galeria');
+function mostrarImagen(){
+    alert('Diste click');
+}
+img.addEventListener('click',mostrarImagen)
+
+
+
+
